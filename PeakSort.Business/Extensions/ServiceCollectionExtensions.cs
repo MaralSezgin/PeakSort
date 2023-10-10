42364d346e90bc2c55cc49e37dcfb2b950939993
sezgin maral <sezginmaral2@gmail.com>
2023-10-10 10:33:02 +0300

    Business Manager yazıldı MVC template yüklendi ve Category veri tabanından çekillerek ekrana yazıldı. DataTable ayarlandı

--- PeakSort.Business/Extensions/ServiceCollectionExtensions.cs
+++ PeakSort.Business/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PeakSort.Business.Abstract;
+using PeakSort.Business.Concrete;
+using PeakSort.DataAccess.Abstract;
+using PeakSort.DataAccess.Concrete;
+using PeakSort.DataAccess.Concrete.EntityFramework.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +14,13 @@ namespace PeakSort.Business.Extensions
 {
    public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection  
+        public static IServiceCollection  LoadServices(this IServiceCollection serviscollection)
+        {
+            serviscollection.AddDbContext<PeaksortContext>();
+            serviscollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviscollection.AddScoped<ICategoryService, CategoryManager>();
+            serviscollection.AddScoped<IProductService, ProductManager>();
+            return serviscollection;
+        }
     }
 }
--- PeakSort.MVCWebUI/Startup.cs
+++ PeakSort.MVCWebUI/Startup.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PeakSort.Business.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+
+
 
 namespace PeakSort.MVCWebUI
 {
@@ -16,6 +20,10 @@ namespace PeakSort.MVCWebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();//sen bir mvc uygulamasý olarak çalýþmalaýsýn
+            services.AddAutoMapper(typeof(Startup));//Automapper kullanmalarýzý düzenler derlenme sýrasýnda
+            services.LoadServices();//bu servisleri biz yazdýk
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +32,22 @@ namespace PeakSort.MVCWebUI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();//bulunmayan bir sayfaya gidince bize 404 döndür
+
             }
 
+            app.UseStaticFiles();//statik file kullanýmý söylüyoruzörnegin resim dosyasý
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints =>//herhanbi bir istek gelince buraya girer
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapAreaControllerRoute(
+                     name: "Admin",
+                     areaName: "Admin",
+                     pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+
+                    );
+                endpoints.MapDefaultControllerRoute();//default olarak home controler ve index e gidr
             });
         }
     }
